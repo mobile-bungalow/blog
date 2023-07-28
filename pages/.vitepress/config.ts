2a93855ff9aa6d02c2b078b18144eada5a4760bc
mobile-bungalow <pwmay@ucsc.edu>
2023-07-28 13:56:52 -0700

    before adding comps

---
+++ pages/.vitepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Concentrated Bursts",
+  description: "the personal site of Paul May."
+})
