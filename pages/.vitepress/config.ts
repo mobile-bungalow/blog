7abd19e51f88e6904b0be0d1a5543d24463acd26
mobile-bungalow <pwmay@ucsc.edu>
2023-08-11 14:40:32 -0700

    add service worker

--- pages/.vitepress/config.ts
+++ pages/.vitepress/config.ts
@@ -2,18 +2,13 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  head: [
+    [ 'script', {src: '/blog/coi-serviceworker.js'} ]
+  ],
   themeConfig: {
     logo: 'public/favicon.ico',
   },
   title: "Concentrated Bursts",
   description: "the personal site of Paul May.",
   base: "/blog",
-  vite: {
-    server: {
-      headers: {
-                "Cross-Origin-Opener-Policy": "same-origin",
-                "Cross-Origin-Embedder-Policy": "require-corp",
-      },
-    },
-  },
 })
